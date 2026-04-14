91ecb90da2ebe310a900ec74d6c2270a034493e0
Jacob Zarnstorff <jacobzarnstorff@gmail.com>
2026-04-13 20:48:19 -0500

    docs: conf.py: Configure project source
    This commit accomplishes a few things including:
      - Add the project's directory to the system path
      - Retrieve the project's version from `pyproject.toml`
      - Replace strings wrapped in single quotes with double quotes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+import sys
+import tomllib
+from pathlib import Path
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,23 +10,31 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PyEEPROM'
-copyright = '2026, Jacob Zarnstorff'
-author = 'Jacob Zarnstorff'
-release = '0.0.1'
+SOURCE_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = SOURCE_DIR.parents[1]
+PYPROJECT_TOML = PROJECT_DIR / "pyproject.toml"
+
+sys.path.insert(0, str(PROJECT_DIR))
+
+with PYPROJECT_TOML.open(mode="rb") as f:
+    data = tomllib.load(f)
+
+project = "PyEEPROM"
+copyright = "%Y, Jacob Zarnstorff"
+author = "Jacob Zarnstorff"
+release = data["project"]["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
